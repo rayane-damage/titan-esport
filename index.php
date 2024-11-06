f162aaf26836897c339f1a4ca034fa98ed3ba73c
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-11-06 12:12:33 +0100

    tournoi infos html + php

--- index.php
+++ index.php
@@ -2,26 +2,31 @@
 <?php if (have_posts()) : ?>
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('hero')?>
+    <?php
+        $tourargs = array(
+            'post_type' => 'tournoi',
+            'posts_per_page' => 1,
+        );
+        $tour_query = new WP_Query( $tourargs );
+        if( $tour_query->have_posts() ): 
+            while( $tour_query->have_posts() ): $tour_query->the_post();
+    ?>
+    <?php get_template_part('tournoi')?>
+    <?php endwhile; ?>
+    <?php endif; ?>  
     <?php get_template_part('team-card')?>
     <?php
         $args = array(
             'post_type' => 'equipes',
             'posts_per_page' => -1,
         );
-        $the_query = new WP_Query( $args );
-        if( $the_query->have_posts() ): 
-            while( $the_query->have_posts() ): $the_query->the_post();
-            $ID = get_the_id();
-            $equipe_1 = get_post_meta($ID, 'equipe_1', true);
+        $team_query = new WP_Query( $args );
+        if( $team_query->have_posts() ): 
+            while( $team_query->have_posts() ): $team_query->the_post();
     ?>
-    <?php get_template_part('match-card', null, array(
-        'id' => $equipe_1,
-    ))
-    
-    ?>
-
-<?php endwhile; ?>
-<?php endif; ?>  
+    <?php get_template_part('match-card')?>
+    <?php endwhile; ?>
+    <?php endif; ?>  
 <?php endwhile; ?>
 <?php endif; ?>  
 <?php get_footer();?>
---
+++ tournoi.php
@@ -0,0 +1,27 @@
+<article class="tournament">
+    <div class="tournament_title">
+        <h2 class="title"><?php the_title() ?></h2>
+        <span class="line"></span>
+    </div>
+    <p class="tournament_desc">
+        <?php the_content()?>
+    </p>
+    <div class="tournament_info">
+        <h3 class="subtitle">infos générales</h3>
+        <ul class="tournament_info-list">
+            <li class="tournament_info-item">Date :</li>
+            <li class="tournament_info-item">Heure :</li>
+            <li class="tournament_info-item">Organisateur :</li>
+        </ul>
+    </div>
+    <div class="tournament_rules">
+        <h3 class="subtitle">règles</h3>
+        <ul class="tournament_rules-list">
+            <li class="tournament_rules-item">Format :</li>
+            <li class="tournament_rules-item">Mode de jeu :</li>
+        </ul>
+    </div>
+
+
+
+</article>
