cbb3e4e3c110c23483628323d3e007a7a92ad1a9
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-11-06 09:07:43 +0100

    match card structure

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <?php while (have_posts()) : the_post(); ?>
     <?php get_template_part('hero')?>
     <?php get_template_part('team-card')?>
+    <?php get_template_part('match-card')?>
 <?php endwhile; ?>
 <?php endif; ?>  
 <?php get_footer();?>
---
+++ match-card.php
@@ -0,0 +1,17 @@
+    <article class="match_card">
+        <div class="match_banner">
+            <figure class="match_banner-img"></figure>
+            <figure class="match_banner-img"></figure>
+        </div>
+        <div class="match_card-teams">
+            <div class="match_card-teamInfos">
+                <h4 class="teamInfos_name"></h4>
+                <h6 class="teamInfos_score"></h6>
+            </div>
+            <h5 class="match_card-vs">VS</h5>
+            <div class="match_card-teamInfos">
+                <h4 class="teamInfos_name"></h4>
+                <h6 class="teamInfos_score"></h6>
+            </div>
+        </div>
+    </article>
