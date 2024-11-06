179f8d2906ec63bb8330294fbbb0360180e1abce
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-11-06 15:52:52 +0100

    fixed match card warning

--- match-card.php
+++ match-card.php
@@ -4,16 +4,16 @@
         </div>
         <div class="match_card-teams">
             <div class="match_card-teamInfos">
-                <a class="teamInfos_name" href="<?php the_permalink(27)?>"><?php $ekip1 = get_field('equipe_1'); 
-                    echo $ekip1;?>
+                <a class="teamInfos_name" href="<?php the_permalink(27)?>">
+                    team name
                 </a>
                 <h6 class="teamInfos_score"><?php $scor = get_field('score'); 
                     echo $scor;?></h6>
             </div>
             <h5 class="match_card-vs">VS</h5>
             <div class="match_card-teamInfos">
-                <a class="teamInfos_name" href="<?php the_permalink(21)?>"><?php $ekip2 = get_field('equipe_2'); 
-                    echo $ekip2;?>
+                <a class="teamInfos_name" href="<?php the_permalink(21)?>">
+                    team name
                 </a>
                 <h6 class="teamInfos_score"><?php $scor2 = get_field('score_2'); 
                     echo $scor2;?></h6>
