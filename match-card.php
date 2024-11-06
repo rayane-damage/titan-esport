5217c77a3034c95df908242d874dc7395fd4d53f
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-11-06 15:37:37 +0100

    match card something that didn't work

--- match-card.php
+++ match-card.php
@@ -4,19 +4,19 @@
         </div>
         <div class="match_card-teams">
             <div class="match_card-teamInfos">
-                <a class="teamInfos_name" href="<?php the_permalink()?>"><?php $ekip1 = get_field('equipe_1'); 
-                    var_dump($ekip1);?>
+                <a class="teamInfos_name" href="<?php the_permalink(27)?>"><?php $ekip1 = get_field('equipe_1'); 
+                    echo $ekip1;?>
                 </a>
                 <h6 class="teamInfos_score"><?php $scor = get_field('score'); 
-                    var_dump($scor);?></h6>
+                    echo $scor;?></h6>
             </div>
             <h5 class="match_card-vs">VS</h5>
             <div class="match_card-teamInfos">
-                <a class="teamInfos_name" href="<?php the_permalink()?>"><?php $ekip2 = get_field('equipe_2'); 
-                    var_dump($ekip2);?>
+                <a class="teamInfos_name" href="<?php the_permalink(21)?>"><?php $ekip2 = get_field('equipe_2'); 
+                    echo $ekip2;?>
                 </a>
-                <h6 class="teamInfos_score"><?php $ekip2 = get_field('score_2'); 
-                    var_dump($scor2);?></h6>
+                <h6 class="teamInfos_score"><?php $scor2 = get_field('score_2'); 
+                    echo $scor2;?></h6>
             </div>
         </div>
     </article>
