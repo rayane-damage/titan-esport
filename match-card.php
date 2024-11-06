a637871963238b6057fd9f7f7c94ce3e687602d4
rayane-damage <150058299+rayane-damage@users.noreply.github.com>
2024-11-06 09:27:42 +0100

    match card php requests

--- match-card.php
+++ match-card.php
@@ -1,17 +1,17 @@
     <article class="match_card">
         <div class="match_banner">
-            <figure class="match_banner-img"></figure>
-            <figure class="match_banner-img"></figure>
+            <figure class="match_banner-img"><?php the_post_thumbnail(); ?></figure>
+            <figure class="match_banner-img"><?php the_post_thumbnail(); ?></figure>
         </div>
         <div class="match_card-teams">
             <div class="match_card-teamInfos">
-                <h4 class="teamInfos_name"></h4>
-                <h6 class="teamInfos_score"></h6>
+                <h4 class="teamInfos_name"><?php the_title( ); ?></h4>
+                <h6 class="teamInfos_score"><?php get_field('score'); ?></h6>
             </div>
             <h5 class="match_card-vs">VS</h5>
             <div class="match_card-teamInfos">
-                <h4 class="teamInfos_name"></h4>
-                <h6 class="teamInfos_score"></h6>
+                <h4 class="teamInfos_name"><?php the_title( ); ?></h4>
+                <h6 class="teamInfos_score"><?php get_field('score'); ?></h6>
             </div>
         </div>
     </article>
